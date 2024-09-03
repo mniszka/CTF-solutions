678253814d668a7f68d228a0818e477efc28b218
mniszka <aflisnik@wp.pl>
2024-09-03 16:43:40 +0200

    Update and rename 13 to 13.py

--- 13.py
+++ 13.py
@@ -1,8 +1,9 @@
+"""
 Description
 Cryptography can be easy, do you know what ROT13 is? 
 cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}
 
-Solution in Python:
+"""
 
 string = "cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}"
 
